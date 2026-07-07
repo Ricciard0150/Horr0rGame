d62e0adce238e85775a442f559aeb10040a5214f
Melissa Zanotto <melissa.angerer@alunos.sc.senac.br>
2026-07-06 22:04:14 -0300

    SettingsButtonsClose

--- Assets/Scripts/HomeScreen/TelaInicial.cs
+++ Assets/Scripts/HomeScreen/TelaInicial.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
     [Header("Painéis")]
     public GameObject painelConfiguracoes;
     public GameObject painelCreditos;
+
+    [Header("Botão Fechar Configurações")]
+    public Image imagemFecharConfiguracoes;
+    public Sprite spriteNormalConfiguracoes;
+    public Sprite spriteVermelhoConfiguracoes;
+
+    [Header("Botão Fechar Créditos")]
+    public Image imagemFecharCreditos;
+    public Sprite spriteNormalCreditos;
+    public Sprite spriteVermelhoCreditos;
+
+    public float tempoAnimacao = 0.15f;
 
     [Header("Cena do Jogo")]
     public string nomeDaCena;
@@ -28,6 +42,16 @@ public class MainMenu : MonoBehaviour
 
     public void FecharConfiguracoes()
     {
+        StartCoroutine(AnimacaoFecharConfiguracoes());
+    }
+
+    IEnumerator AnimacaoFecharConfiguracoes()
+    {
+        imagemFecharConfiguracoes.sprite = spriteVermelhoConfiguracoes;
+
+        yield return new WaitForSeconds(tempoAnimacao);
+
+        imagemFecharConfiguracoes.sprite = spriteNormalConfiguracoes;
         painelConfiguracoes.SetActive(false);
     }
 
@@ -38,6 +62,16 @@ public class MainMenu : MonoBehaviour
 
     public void FecharCreditos()
     {
+        StartCoroutine(AnimacaoFecharCreditos());
+    }
+
+    IEnumerator AnimacaoFecharCreditos()
+    {
+        imagemFecharCreditos.sprite = spriteVermelhoCreditos;
+
+        yield return new WaitForSeconds(tempoAnimacao);
+
+        imagemFecharCreditos.sprite = spriteNormalCreditos;
         painelCreditos.SetActive(false);
     }
     public void SairDoJogo()
